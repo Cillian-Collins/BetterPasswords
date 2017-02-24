303fb7a20a5b172c245d1f59ff042c64cce33472
Cillian <Cilliancollins2002@gmail.com>
2017-02-24 13:43:27 +0000

    Uploaded BetterPasswords
    Version 1.00 is in the form of a simple batch file which can be ran by all Windows users.

---
+++ BetterPasswords.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+set /p id="Enter Your Password: "
+
+if defined id if "%id:~5,1%"=="" (
+    echo Your passsword must be more than 6 characters.
+    echo Please open the batch file again!
+    pause
+    exit
+) else (
+    
+
+
+echo Success! Loading...
+pause
+echo Your NEW Password: #%id%%random:~-1%%random:~-1%%random:~-1%%random:~-1%
+echo #%id%%random:~-1%%random:~-1%%random:~-1%%random:~-1% >> Password.txt
+start notepad "Password.txt"
+echo Please find your new password in Password.txt
+pause )
