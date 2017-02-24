5620b822007f187465d2bc9bea6439715661c4b9
Cillian <Cilliancollins2002@gmail.com>
2017-02-24 17:47:58 +0000

    Python Version
    Added python compatibility (an alternative for users not using Windows OS or cannot open a batch file).
    
    Requires: Python to be installed.

---
+++ BetterPasswords.py
@@ -0,0 +1,21 @@
+from random import randint
+import subprocess as sp
+
+word = str(raw_input('Enter your password here: '))
+
+while len(word) < 6 or not word.replace(' ', '').isalpha():
+    if len(word) < 6:
+        print("Please enter a word larger than 6 characters long!")
+    else:
+        print("Please enter letters only!")
+
+    word = raw_input('Enter your password here: ')
+
+print '#' + word + str(randint(1000,9999))
+with open('passwords.txt', 'wb') as file:
+    file.write('#' + word + str(randint(1000,9999)))
+
+programName = "notepad.exe"
+fileName = "passwords.txt"
+sp.Popen([programName, fileName])
+    
