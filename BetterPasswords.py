10c162998e0c58e2f398ff0fd5ddc9a93097eeeb
Cillian <Cilliancollins2002@gmail.com>
2017-03-08 16:54:50 +0000

    Version 1.01
    Added comments, fixed general errors and increased speed/accuracy.

--- BetterPasswords.py
+++ BetterPasswords.py
@@ -1,6 +1,10 @@
+#Python Password Generator Version 1.01
+
+#Imports
 from random import randint
 import subprocess as sp
 
+#Copyrights
 print("+=====================================================================================================================+")
 print("|                                                                                                                     |")
 print("|                                                  BETTER PASSWORDS                                                   |")
@@ -8,21 +12,34 @@ print("|                                                 by Cillian Collins     
 print("|                                                                                                                     |")
 print("+=====================================================================================================================+")
 
+#Input
 word = str(raw_input('Enter your password here: '))
 
+#Alpha only and less than 6 character long loop
 while len(word) < 6 or not word.replace(' ', '').isalpha():
+    #Check if less than 6 characters
     if len(word) < 6:
+        #If less than 6 characters, print the error
         print("Please enter a word larger than 6 characters long!")
     else:
+        #If more than 5 characters it must not be letters only, so print the error
         print("Please enter letters only!")
-
+    #If it threw an error, prompt the user to fix the input error
     word = raw_input('Enter your password here: ')
 
-print '#' + word + str(randint(1000,9999))
+#Formula for the password generator saved as variable
+password = '#' + word + str(randint(1000,9999))
+#Prints the variable
+print (password)
+#Saves a passwords.txt file
 with open('passwords.txt', 'wb') as file:
-    file.write('#' + word + str(randint(1000,9999)))
+    #Writes the 'password' variable to the file
+    file.write(password)
 
+#Sets variable for notepad.exe
 programName = "notepad.exe"
+#Sets variable for passwords.txt
 fileName = "passwords.txt"
+#Opens passwords.txt in notepad.exe
 sp.Popen([programName, fileName])
     
